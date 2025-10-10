9deb2b73e1ae8aac98b148169a936398130a753e
Junzhe <jshi70@jh.edu>
2025-10-10 18:03:36 -0400

    Albert Wang complete input validation

--- main.cpp
+++ main.cpp
@@ -35,10 +35,51 @@ int main(int argc, char **argv)
     return 1;
   }
   // sets blocks block_size power of 2
+  if ((sets & (sets - 1)) != 0)
+  {
+    std::cerr << "Error: Number of sets must be a power of 2" << std::endl;
+    return 1;
+  }
+
+  if ((blocks & (blocks - 1)) != 0)
+  {
+    std::cerr << "Error: Number of blocks per set must be a power of 2" << std::endl;
+    return 1;
+  }
+
+  if ((block_size & (block_size - 1)) != 0)
+  {
+    std::cerr << "Error: Block size must be a power of 2" << std::endl;
+    return 1;
+  }
 
   // write alloc can only be write-allocate or no-write-allocate
+  if (write_alloc != "write-allocate" && write_alloc != "no-write-allocate")
+  {
+    std::cerr << "Error: Write allocation policy must be 'write-allocate' or 'no-write-allocate'" << std::endl;
+    return 1;
+  }
 
   // write policy can only be write-through or write-back
+  if (write_policy != "write-through" && write_policy != "write-back")
+  {
+    std::cerr << "Error: Write policy must be 'write-through' or 'write-back'" << std::endl;
+    return 1;
+  }
+
+  // replacement policy can only be lru or fifo
+  if (replacement_policy != "lru" && replacement_policy != "fifo")
+  {
+    std::cerr << "Error: Replacement policy must be 'lru' or 'fifo'" << std::endl;
+    return 1;
+  }
+
+  // Invalid combination: write-back and no-write-allocate
+  if (write_policy == "write-back" && write_alloc == "no-write-allocate")
+  {
+    std::cerr << "Error: Cannot combine 'write-back' with 'no-write-allocate'" << std::endl;
+    return 1;
+  }
 
   // std out - hard-coded, will be replaced with variables in the future, currently as a start up.
   std::cout << "Total loads: 0" << std::endl;
